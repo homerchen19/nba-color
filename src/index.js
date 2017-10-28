21dcf12c40e47d9f067dad745b3b23ea04434439
xxhomey19 <xxhomey19@gmail.com>
2017-10-28 13:30:09 +0800

    Allow user send lowercase team abbreviation

--- src/index.js
+++ src/index.js
@@ -3,32 +3,36 @@ import NbaColors from './colors';
 const getAllColors = () => NbaColors;
 
 const getMainColor = teamAbbreviation => {
-  if (NbaColors[teamAbbreviation]) {
-    return NbaColors[teamAbbreviation].mainColor;
+  const abbreviation = teamAbbreviation.toUpperCase();
+  if (NbaColors[abbreviation]) {
+    return NbaColors[abbreviation].mainColor;
   }
 
   return undefined;
 };
 
 const getColors = teamAbbreviation => {
-  if (NbaColors[teamAbbreviation]) {
-    return NbaColors[teamAbbreviation].colors;
+  const abbreviation = teamAbbreviation.toUpperCase();
+  if (NbaColors[abbreviation]) {
+    return NbaColors[abbreviation].colors;
   }
 
   return undefined;
 };
 
 const getColorsList = teamAbbreviation => {
-  if (NbaColors[teamAbbreviation]) {
-    return Object.keys(NbaColors[teamAbbreviation].colors);
+  const abbreviation = teamAbbreviation.toUpperCase();
+  if (NbaColors[abbreviation]) {
+    return Object.keys(NbaColors[abbreviation].colors);
   }
 
   return undefined;
 };
 
 const getFullName = teamAbbreviation => {
-  if (NbaColors[teamAbbreviation]) {
-    return NbaColors[teamAbbreviation].fullName;
+  const abbreviation = teamAbbreviation.toUpperCase();
+  if (NbaColors[abbreviation]) {
+    return NbaColors[abbreviation].fullName;
   }
 
   return undefined;
