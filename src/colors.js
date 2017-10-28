8d292bf2c899c6d019009f79174e6cb327bba171
kpman <s92f002@hotmail.com>
2017-10-28 12:59:22 +0800

    Uppercase to lowercase

--- src/colors.js
+++ src/colors.js
@@ -12,21 +12,21 @@ const colors = {
   ATL: {
     fullName: 'Atlanta Hawks',
     mainColor: {
-      hex: '#C8102E',
+      hex: '#c8102e',
       rgb: [200, 16, 46],
     },
     colors: {
       red: {
-        hex: '#C8102E',
+        hex: '#c8102e',
         rgb: [200, 16, 46],
       },
       green: {
-        hex: '#C4D600',
+        hex: '#c4d600',
         rgb: [196, 214, 0],
       },
       white: pureWhite,
       gray: {
-        hex: '#25282A',
+        hex: '#25282a',
         rgb: [37, 40, 42],
       },
     },
@@ -48,22 +48,22 @@ const colors = {
   BOS: {
     fullName: 'Boston Celtics',
     mainColor: {
-      hex: '#007A33',
+      hex: '#007a33',
       rgb: [0, 122, 51],
     },
     colors: {
       green: {
-        hex: '#007A33',
+        hex: '#007a33',
         rgb: [0, 122, 51],
       },
       black: pureBlack,
       white: pureWhite,
       gold: {
-        hex: '#FFD700',
+        hex: '#ffd700',
         rgb: [255, 215, 0],
       },
       silver: {
-        hex: '#C0C0C0',
+        hex: '#c0c0c0',
         rgb: [192, 192, 192],
       },
     },
@@ -80,12 +80,12 @@ const colors = {
         rgb: [32, 23, 71],
       },
       teal: {
-        hex: '#00778B',
+        hex: '#00778b',
         rgb: [0, 119, 139],
       },
       white: pureWhite,
       gray: {
-        hex: '#888B8D',
+        hex: '#888b8d',
         rgb: [136, 139, 141],
       },
     },
@@ -93,12 +93,12 @@ const colors = {
   CHI: {
     fullName: 'Chicago Bulls',
     mainColor: {
-      hex: '#BA0C2F',
+      hex: '#ba0c2f',
       rgb: [186, 12, 47],
     },
     colors: {
       red: {
-        hex: '#BA0C2F',
+        hex: '#ba0c2f',
         rgb: [186, 12, 47],
       },
       black: pureBlack,
@@ -108,21 +108,21 @@ const colors = {
   CLE: {
     fullName: 'Cleveland Cavaliers',
     mainColor: {
-      hex: '#6F263D',
+      hex: '#6f263d',
       rgb: [111, 38, 61],
     },
     colors: {
       wine: {
-        hex: '#6F263D',
+        hex: '#6f263d',
         rgb: [111, 38, 61],
       },
       white: pureWhite,
       navy: {
-        hex: '#041E42',
+        hex: '#041e42',
         rgb: [4, 30, 66],
       },
       gold: {
-        hex: '#FFB81C',
+        hex: '#ffb81c',
         rgb: [255, 184, 28],
       },
     },
@@ -130,20 +130,20 @@ const colors = {
   DAL: {
     fullName: 'Dallas Mavericks',
     mainColor: {
-      hex: '#0050B5',
+      hex: '#0050b5',
       rgb: [0, 80, 181],
     },
     colors: {
       blue: {
-        hex: '#0050B5',
+        hex: '#0050b5',
         rgb: [0, 80, 181],
       },
       silver: {
-        hex: '#8D9093',
+        hex: '#8d9093',
         rgb: [141, 144, 147],
       },
       navy: {
-        hex: '#0C2340',
+        hex: '#0c2340',
         rgb: [12, 35, 64],
       },
       black: pureBlack,
@@ -153,20 +153,20 @@ const colors = {
   DEN: {
     fullName: 'Denver Nuggets',
     mainColor: {
-      hex: '#418FDE',
+      hex: '#418fde',
       rgb: [65, 143, 222],
     },
     colors: {
       blue: {
-        hex: '#418FDE',
+        hex: '#418fde',
         rgb: [65, 143, 222],
       },
       gold: {
-        hex: '#FFC72C',
+        hex: '#ffc72c',
         rgb: [255, 199, 44],
       },
       navy: {
-        hex: '#0C2340',
+        hex: '#0c2340',
         rgb: [12, 35, 64],
       },
       white: pureWhite,
@@ -175,20 +175,20 @@ const colors = {
   DET: {
     fullName: 'Detroit Pistons',
     mainColor: {
-      hex: '#003DA5',
+      hex: '#003da5',
       rgb: [0, 61, 165],
     },
     colors: {
       blue: {
-        hex: '#003DA5',
+        hex: '#003da5',
         rgb: [0, 61, 165],
       },
       red: {
-        hex: '#D50032',
+        hex: '#d50032',
         rgb: [213, 0, 50],
       },
       navy: {
-        hex: '#041E42',
+        hex: '#041e42',
         rgb: [4, 30, 66],
       },
       white: pureWhite,
@@ -197,16 +197,16 @@ const colors = {
   GSW: {
     fullName: 'Golden State Warriors',
     mainColor: {
-      hex: '#FFC72D',
+      hex: '#ffc72d',
       rgb: [255, 199, 44],
     },
     colors: {
       gold: {
-        hex: '#FFC72D',
+        hex: '#ffc72d',
         rgb: [255, 199, 44],
       },
       blue: {
-        hex: '#003DA5',
+        hex: '#003da5',
         rgb: [0, 61, 165],
       },
     },
@@ -214,20 +214,20 @@ const colors = {
   HOU: {
     fullName: 'Houston Rockets',
     mainColor: {
-      hex: '#BA0C2F',
+      hex: '#ba0c2f',
       rgb: [186, 12, 47],
     },
     colors: {
       red: {
-        hex: '#BA0C2F',
+        hex: '#ba0c2f',
         rgb: [186, 12, 47],
       },
       silver: {
-        hex: '#8D9093',
+        hex: '#8d9093',
         rgb: [141, 144, 147],
       },
       yellow: {
-        hex: '#FDB927',
+        hex: '#fdb927',
         rgb: [253, 185, 39],
       },
       white: pureWhite,
@@ -237,20 +237,20 @@ const colors = {
   IND: {
     fullName: 'Indiana Pacers',
     mainColor: {
-      hex: '#041E42',
+      hex: '#041e42',
       rgb: [4, 30, 66],
     },
     colors: {
       blue: {
-        hex: '#041E42',
+        hex: '#041e42',
         rgb: [4, 30, 66],
       },
       gold: {
-        hex: '#FFB81C',
+        hex: '#ffb81c',
         rgb: [255, 184, 28],
       },
       silver: {
-        hex: '#B1B3B3',
+        hex: '#b1b3b3',
         rgb: [177, 179, 179],
       },
       white: pureWhite,
@@ -259,20 +259,20 @@ const colors = {
   LAC: {
     fullName: 'Los Angeles Clippers',
     mainColor: {
-      hex: '#D50032',
+      hex: '#d50032',
       rgb: [213, 0, 50],
     },
     colors: {
       red: {
-        hex: '#D50032',
+        hex: '#d50032',
         rgb: [213, 0, 50],
       },
       blue: {
-        hex: '#003DA5',
+        hex: '#003da5',
         rgb: [0, 61, 165],
       },
       silver: {
-        hex: '#B1B3B3',
+        hex: '#b1b3b3',
         rgb: [177, 179, 179],
       },
       white: pureWhite,
@@ -281,16 +281,16 @@ const colors = {
   LAL: {
     fullName: 'Los Angeles Lakers',
     mainColor: {
-      hex: '#702F8A',
+      hex: '#702f8a',
       rgb: [112, 47, 138],
     },
     colors: {
       purple: {
-        hex: '#702F8A',
+        hex: '#702f8a',
         rgb: [112, 47, 138],
       },
       gold: {
-        hex: '#FFC72C',
+        hex: '#ffc72c',
         rgb: [255, 199, 44],
       },
       white: pureWhite,
@@ -299,24 +299,24 @@ const colors = {
   MEM: {
     fullName: 'Memphis Grizzlies',
     mainColor: {
-      hex: '#23375B',
+      hex: '#23375b',
       rgb: [35, 55, 91],
     },
     colors: {
       midnightBlue: {
-        hex: '#23375B',
+        hex: '#23375b',
         rgb: [35, 55, 91],
       },
       bealeStreetBlue: {
-        hex: '#6189B9',
+        hex: '#6189b9',
         rgb: [97, 137, 185],
       },
       smokeBlue: {
-        hex: '#BBD1E4',
+        hex: '#bbd1e4',
         rgb: [187, 209, 228],
       },
       gold: {
-        hex: '#FFD432',
+        hex: '#ffd432',
         rgb: [255, 215, 50],
       },
     },
@@ -333,7 +333,7 @@ const colors = {
         rgb: [134, 38, 51],
       },
       yellow: {
-        hex: '#FFA300',
+        hex: '#ffa300',
         rgb: [255, 163, 0],
       },
       white: pureWhite,
@@ -343,20 +343,20 @@ const colors = {
   MIL: {
     fullName: 'Milwaukee Bucks',
     mainColor: {
-      hex: '#2C5234',
+      hex: '#2c5234',
       rgb: [44, 82, 52],
     },
     colors: {
       green: {
-        hex: '#2C5234',
+        hex: '#2c5234',
         rgb: [44, 82, 52],
       },
       cream: {
-        hex: '#DDCBA4',
+        hex: '#ddcba4',
         rgb: [221, 203, 164],
       },
       blue: {
-        hex: '#0057B8',
+        hex: '#0057b8',
         rgb: [0, 87, 184],
       },
       white: pureWhite,
@@ -366,20 +366,20 @@ const colors = {
   MIN: {
     fullName: 'Minnesota Timberwolves',
     mainColor: {
-      hex: '#002B5C',
+      hex: '#002b5c',
       rgb: [0, 42, 92],
     },
     colors: {
       blue: {
-        hex: '#002B5C',
+        hex: '#002b5c',
         rgb: [0, 42, 92],
       },
       grey: {
-        hex: '#C6CFD4',
+        hex: '#c6cfd4',
         rgb: [198, 208, 215],
       },
       green: {
-        hex: '#7AC143',
+        hex: '#7ac143',
         rgb: [122, 192, 67],
       },
       white: pureWhite,
@@ -388,20 +388,20 @@ const colors = {
   NOP: {
     fullName: 'New Orleans Pelicans',
     mainColor: {
-      hex: '#002B5C',
+      hex: '#002b5c',
       rgb: [0, 43, 92],
     },
     colors: {
       blue: {
-        hex: '#002B5C',
+        hex: '#002b5c',
         rgb: [0, 43, 92],
       },
       gold: {
-        hex: '#B4975A',
+        hex: '#b4975a',
         rgb: [180, 151, 90],
       },
       red: {
-        hex: '#E31937',
+        hex: '#e31937',
         rgb: [227, 25, 55],
       },
       white: pureWhite,
@@ -410,20 +410,20 @@ const colors = {
   NYK: {
     fullName: 'New York Knicks',
     mainColor: {
-      hex: '#003DA5',
+      hex: '#003da5',
       rgb: [0, 61, 165],
     },
     colors: {
       blue: {
-        hex: '#003DA5',
+        hex: '#003da5',
         rgb: [0, 61, 165],
       },
       orange: {
-        hex: '#FF671F',
+        hex: '#ff671f',
         rgb: [255, 103, 31],
       },
       silver: {
-        hex: '#B1B3B3',
+        hex: '#b1b3b3',
         rgb: [177, 179, 179],
       },
       white: pureWhite,
@@ -432,24 +432,24 @@ const colors = {
   OKC: {
     fullName: 'Oklahoma City Thunder',
     mainColor: {
-      hex: '#007DC3',
+      hex: '#007dc3',
       rgb: [0, 125, 195],
     },
     colors: {
       blue: {
-        hex: '#007DC3',
+        hex: '#007dc3',
         rgb: [0, 125, 195],
       },
       orange: {
-        hex: '#F05133',
+        hex: '#f05133',
         rgb: [240, 81, 51],
       },
       yellow: {
-        hex: '#FDBB30',
+        hex: '#fdbb30',
         rgb: [253, 187, 48],
       },
       darkBlue: {
-        hex: '#002D62',
+        hex: '#002d62',
         rgb: [0, 45, 98],
       },
     },
@@ -457,16 +457,16 @@ const colors = {
   ORL: {
     fullName: 'Orlando Magic',
     mainColor: {
-      hex: '#007DC5',
+      hex: '#007dc5',
       rgb: [0, 125, 197],
     },
     colors: {
       blue: {
-        hex: '#007DC5',
+        hex: '#007dc5',
         rgb: [0, 125, 197],
       },
       silver: {
-        hex: '#C4CED3',
+        hex: '#c4ced3',
         rgb: [196, 206, 211],
       },
       white: pureWhite,
@@ -476,16 +476,16 @@ const colors = {
   PHI: {
     fullName: 'Philadelphia 76ers',
     mainColor: {
-      hex: '#006BB6',
+      hex: '#006bb6',
       rgb: [0, 102, 182],
     },
     colors: {
       blue: {
-        hex: '#006BB6',
+        hex: '#006bb6',
         rgb: [0, 102, 182],
       },
       red: {
-        hex: '#ED174C',
+        hex: '#ed174c',
         rgb: [237, 23, 76],
       },
       white: pureWhite,
@@ -494,20 +494,20 @@ const colors = {
   PHX: {
     fullName: 'Phoenix Suns',
     mainColor: {
-      hex: '#E56020',
+      hex: '#e56020',
       rgb: [229, 96, 32],
     },
     colors: {
       orange: {
-        hex: '#E56020',
+        hex: '#e56020',
         rgb: [229, 96, 32],
       },
       purple: {
-        hex: '#1D1160',
+        hex: '#1d1160',
         rgb: [29, 17, 96],
       },
       gray: {
-        hex: '#63717A',
+        hex: '#63717a',
         rgb: [99, 113, 122],
       },
       white: pureWhite,
@@ -517,16 +517,16 @@ const colors = {
   POR: {
     fullName: 'Portland Trailblazers',
     mainColor: {
-      hex: '#F0163A',
+      hex: '#f0163a',
       rgb: [240, 22, 58],
     },
     colors: {
       red: {
-        hex: '#F0163A',
+        hex: '#f0163a',
         rgb: [240, 22, 58],
       },
       silver: {
-        hex: '#B6BFBF',
+        hex: '#b6bfbf',
         rgb: [182, 191, 191],
       },
       white: pureWhite,
@@ -536,16 +536,16 @@ const colors = {
   SAC: {
     fullName: 'Sacramento Kings',
     mainColor: {
-      hex: '#724C9F',
+      hex: '#724c9f',
       rgb: [114, 76, 159],
     },
     colors: {
       purple: {
-        hex: '#724C9F',
+        hex: '#724c9f',
         rgb: [114, 76, 159],
       },
       silver: {
-        hex: '#8E9090',
+        hex: '#8e9090',
         rgb: [142, 144, 144],
       },
       white: pureWhite,
@@ -555,12 +555,12 @@ const colors = {
   SAS: {
     fullName: 'San Antonio Spurs',
     mainColor: {
-      hex: '#B6BFBF',
+      hex: '#b6bfbf',
       rgb: [182, 191, 191],
     },
     colors: {
       silver: {
-        hex: '#B6BFBF',
+        hex: '#b6bfbf',
         rgb: [182, 191, 191],
       },
       white: pureWhite,
@@ -570,16 +570,16 @@ const colors = {
   TOR: {
     fullName: 'Toronto Raptors',
     mainColor: {
-      hex: '#CE1141',
+      hex: '#ce1141',
       rgb: [206, 17, 65],
     },
     colors: {
       red: {
-        hex: '#CE1141',
+        hex: '#ce1141',
         rgb: [206, 17, 65],
       },
       silver: {
-        hex: '#C4CED3',
+        hex: '#c4ced3',
         rgb: [196, 206, 211],
       },
       white: pureWhite,
@@ -589,24 +589,24 @@ const colors = {
   UTA: {
     fullName: 'Utah Jazz',
     mainColor: {
-      hex: '#002B5C',
+      hex: '#002b5c',
       rgb: [0, 43, 92],
     },
     colors: {
       navy: {
-        hex: '#002B5C',
+        hex: '#002b5c',
         rgb: [0, 43, 92],
       },
       yellow: {
-        hex: '#F9A01B',
+        hex: '#f9a01b',
         rgb: [249, 160, 27],
       },
       green: {
-        hex: '#00471B',
+        hex: '#00471b',
         rgb: [0, 71, 27],
       },
       gray: {
-        hex: '#BEC0C2',
+        hex: '#bec0c2',
         rgb: [190, 192, 194],
       },
     },
@@ -614,20 +614,20 @@ const colors = {
   WAS: {
     fullName: 'Washington Wizards',
     mainColor: {
-      hex: '#0C2340',
+      hex: '#0c2340',
       rgb: [12, 35, 64],
     },
     colors: {
       navy: {
-        hex: '#0C2340',
+        hex: '#0c2340',
         rgb: [12, 35, 64],
       },
       red: {
-        hex: '#C8102E',
+        hex: '#c8102e',
         rgb: [200, 16, 46],
       },
       silver: {
-        hex: '#8D9093',
+        hex: '#8d9093',
         rgb: [141, 144, 147],
       },
       white: pureWhite,
