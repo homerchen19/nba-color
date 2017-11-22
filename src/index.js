6e9904b95d38e14677138ec817a9f50220cf9380
xxhomey19 <xxhomey19@gmail.com>
2017-11-22 16:45:17 +0800

    Refactor and add ramda

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
+import R from 'ramda';
+
 import NbaColors from './colors';
+
+const Team = teamAbbreviation => ({
+  toUpperCase: () => Team(R.toUpper(teamAbbreviation)),
+  getMainColor: () => R.prop('mainColor', NbaColors[teamAbbreviation]),
+  getColors: () => R.prop('colors', NbaColors[teamAbbreviation]),
+  getColorsList: () => {
+    const getColorsKeys = R.compose(R.keys, R.prop('colors'));
+    const checkEmpty = R.ifElse(R.isEmpty(), () => undefined, R.identity());
+
+    return R.compose(checkEmpty, getColorsKeys)(NbaColors[teamAbbreviation]);
+  },
+  getFullName: () => R.prop('fullName', NbaColors[teamAbbreviation]),
+});
 
 const getAllColors = () => NbaColors;
 
-const getMainColor = teamAbbreviation => {
-  const abbreviation = teamAbbreviation.toUpperCase();
-  if (NbaColors[abbreviation]) {
-    return NbaColors[abbreviation].mainColor;
-  }
+const getMainColor = teamAbbreviation =>
+  Team(teamAbbreviation)
+    .toUpperCase()
+    .getMainColor();
 
-  return undefined;
-};
+const getColors = teamAbbreviation =>
+  Team(teamAbbreviation)
+    .toUpperCase()
+    .getColors();
 
-const getColors = teamAbbreviation => {
-  const abbreviation = teamAbbreviation.toUpperCase();
-  if (NbaColors[abbreviation]) {
-    return NbaColors[abbreviation].colors;
-  }
+const getColorsList = teamAbbreviation =>
+  Team(teamAbbreviation)
+    .toUpperCase()
+    .getColorsList();
 
-  return undefined;
-};
-
-const getColorsList = teamAbbreviation => {
-  const abbreviation = teamAbbreviation.toUpperCase();
-  if (NbaColors[abbreviation]) {
-    return Object.keys(NbaColors[abbreviation].colors);
-  }
-
-  return undefined;
-};
-
-const getFullName = teamAbbreviation => {
-  const abbreviation = teamAbbreviation.toUpperCase();
-  if (NbaColors[abbreviation]) {
-    return NbaColors[abbreviation].fullName;
-  }
-
-  return undefined;
-};
+const getFullName = teamAbbreviation =>
+  Team(teamAbbreviation)
+    .toUpperCase()
+    .getFullName();
 
 module.exports = {
   getAllColors,
