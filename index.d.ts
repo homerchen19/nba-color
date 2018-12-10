d88dd59308c218ee413083f498a81f311aad2b6d
xxhomey19 <xxhomey19@gmail.com>
2018-12-10 22:55:53 +1100

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+interface teamColor {
+  hex: string;
+  tgb: [number, number, number];
+}
+
+interface teamColors {
+  [colorName: string]: teamColor;
+}
+
+interface colors {
+  [teamAbbreviation: string]: {
+    fullName: string;
+    colors: teamColors;
+  };
+}
+
+export function getAllColors(): colors;
+export function getMainColor(teamAbbreviation: string): teamColor;
+export function getColors(teamAbbreviation: string): teamColors;
+export function getFullName(teamAbbreviation: string): string;
+export function getColorsList(teamAbbreviation: string): string[];
